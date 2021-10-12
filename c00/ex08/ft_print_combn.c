c106e91b60bddb1412822849335655743e82b20a
gyukim <lynnfield3577@gmail.com>
2021-10-12 17:07:30 +0900

    1

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: gyukim <gyukim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 16:17:40 by gyukim            #+#    #+#             */
-/*   Updated: 2021/10/11 16:49:46 by gyukim           ###   ########.fr       */
+/*   Updated: 2021/10/12 17:02:09 by gyukim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,48 +16,70 @@ void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
-void	ft_print_tochar(int arr_n[], int index, int n)
+void	ft_print_arr(int arr[], int n)
 {
-	int i;
-	i = 0;
-	while(i < n)
-	{
-		ft_putchar(arr_n[index]+48);
-		i++;
-	}
+	int index;
+	index = 0;
+	while(index < n) ft_putchar(arr[index++]+48);
 	ft_putchar(',');
 	ft_putchar(' ');
 }
-void	ft_int_tab(int arr_n[], int index)
+
+void	ft_reset(int arr[], int index, int n)
 {
-	if (arr_n[index]>=9)
-	{
-		arr_n[index] = 0;
-	}
-	else
-	{
-		arr_n[index]++;
-
-	}
-}
-
-void	ft_print_combn(int n)
-{
-	int arr_n[10];
-	int index;
-
-	index = 10;
-	while(index>0)
-	{
-		index--;
-		arr_n[index] = 0;
-	}
-	index = 0;
-	
 	while(index < n)
 	{
-		ft_int_tab(arr_n, index);
-		ft_print_tochar(arr_n, index, n);
+		arr[index+1] = arr[index]+1;
 		index++;
+	}
+}
+int	ft_check(int arr[], int index, int n)
+{
+	int max;
+	max = 9;
+	while(n > index) {
+		if(arr[n] == max)
+		{
+			n--;
+			max--;
+		}
+		else return n, max;
+	}
+	
+	return 0;
+}
+void	ft_print_combn(int n)
+{
+	int arr[n];
+	int index;
+	int max;
+
+	index = 0;
+	max = 9;
+
+	while(index < n)
+	{
+		arr[index] = index;
+		index++;
+	}
+
+	index--;
+	ft_print_arr(arr, n);
+	while(index > -1)
+	{
+		if(arr[index] < max) arr[index]++;
+		else 
+		{
+			if(index>0) --index, --max;
+			else return;
+
+			arr[index]++;
+			ft_reset(arr, index, n);
+			if(ft_check(arr, index, n) > 0)
+			{
+				index, max = ft_check(arr, index, n);
+			}
+		}
+		ft_print_arr(arr, n);
 	}
 }

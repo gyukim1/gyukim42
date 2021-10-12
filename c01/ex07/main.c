b5a0f9d1e4c43f550b70c61394aa8c764cec2c79
gyukim <lynnfield3577@gmail.com>
2021-10-12 18:04:31 +0900

    1

---
+++ c01/ex07/main.c
@@ -0,0 +1,6 @@
+void ft_rev_int_tab(int *arr, int size);
+int main(void)
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	ft_rev_int_tab(arr, 9);
+}
---
+++ c01/ex08/main.c
@@ -0,0 +1,8 @@
+void ft_sort_int_tab(int *tab, int size);
+
+int main(void)
+{
+	int tab[9] = { 8, 2, 1, 5, 4, 9, 3, 7, 6 };
+	ft_sort_int_tab(tab, 9);
+}
+
